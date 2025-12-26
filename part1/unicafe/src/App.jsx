e89cb0ccffec60fd95adbbb030ecce25285d9418
Jettan17 <jethrotan2006@gmail.com>
2025-12-26 20:41:56 +0800

    part1 ex6

---
+++ part1/unicafe/src/App.jsx
@@ -0,0 +1,30 @@
+import { useState } from 'react'
+
+const FeedbackButton = ({handleClick, type}) => {
+  return (
+    <button onClick={handleClick}>{type}</button>
+  )
+}
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <FeedbackButton handleClick={() => setGood(good + 1)} type="good" />
+      <FeedbackButton handleClick={() => setNeutral(neutral+1)} type="neutral" />
+      <FeedbackButton handleClick={() => setBad(bad + 1)} type="bad" />
+      <h1>Statistics</h1>
+
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+    </div>
+  )
+}
+
+export default App
