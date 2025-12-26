b0ebc4972fc3334f92edb423daa847377101a5a5
Jettan17 <jethrotan2006@gmail.com>
2025-12-26 20:45:43 +0800

    feat: part1 ex7

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -20,9 +20,12 @@ const App = () => {
       <FeedbackButton handleClick={() => setBad(bad + 1)} type="bad" />
       <h1>Statistics</h1>
 
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
+      <p>good {good} </p>
+      <p>neutral {neutral} </p>
+      <p>bad {bad} </p>
+      <p>all {good+neutral+bad} </p>
+      <p>average {(good - bad) / (good+neutral+bad)} </p>
+      <p>positive {good / (good+neutral+bad) * 100}% </p>
     </div>
   )
 }
