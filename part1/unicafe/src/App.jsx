7b13a1a373a9f24717c3b78622ab7f0337ddced1
Jettan17 <jethrotan2006@gmail.com>
2025-12-26 20:55:50 +0800

    feat: part1 ex8

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -6,6 +6,20 @@ const FeedbackButton = ({handleClick, type}) => {
   )
 }
 
+const Statistics = ({good, neutral, bad}) => {
+  return (
+    <>
+    <h1>Statistics</h1>
+    <p>good {good} </p>
+    <p>neutral {neutral} </p>
+    <p>bad {bad} </p>
+    <p>all {good + neutral + bad} </p>
+    <p>average {(good - bad) / (good + neutral + bad)} </p>
+    <p>positive {good / (good + neutral + bad) * 100}% </p>
+    </>
+  )
+}
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
@@ -18,14 +32,7 @@ const App = () => {
       <FeedbackButton handleClick={() => setGood(good + 1)} type="good" />
       <FeedbackButton handleClick={() => setNeutral(neutral+1)} type="neutral" />
       <FeedbackButton handleClick={() => setBad(bad + 1)} type="bad" />
-      <h1>Statistics</h1>
-
-      <p>good {good} </p>
-      <p>neutral {neutral} </p>
-      <p>bad {bad} </p>
-      <p>all {good+neutral+bad} </p>
-      <p>average {(good - bad) / (good+neutral+bad)} </p>
-      <p>positive {good / (good+neutral+bad) * 100}% </p>
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   )
 }
