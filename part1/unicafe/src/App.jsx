825b804b439fe5397bc8da84889c8f38a96d81d4
Jettan17 <jethrotan2006@gmail.com>
2025-12-26 20:58:27 +0800

    feat: part1 ex9

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -7,6 +7,11 @@ const FeedbackButton = ({handleClick, type}) => {
 }
 
 const Statistics = ({good, neutral, bad}) => {
+  if (good === 0 && neutral === 0 && bad === 0) {
+    return (
+      <p>No feedback given</p>
+    )
+  }
   return (
     <>
     <h1>Statistics</h1>
